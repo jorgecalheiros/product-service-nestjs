ba8dce8d8d0c8d6cbb7aff6d8f170b275eb4aa13
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-01 16:23:01 -0300

    documentação da api de produtos

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Products Service')
+    .setDescription('Products service api')
+    .setVersion('0.1')
+    .addTag('Products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
