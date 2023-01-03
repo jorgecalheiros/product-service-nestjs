d601729e8545584d53d76104a91f501d34e4f4f8
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-02 23:51:30 -0300

    change develop

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ConsumerService } from './infra/messaging/kafka/consumer/consumer.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-
-  const consumerService = app.get(ConsumerService)
-
-  app.connectMicroservice<MicroserviceOptions>({
-    strategy: consumerService
-  })
 
   const config = new DocumentBuilder()
     .setTitle('Product Service')
