8a1245bf6d309285f18755c45c4318ba9faf000f
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-03 01:16:59 -0300

    setup kafka

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,39 @@
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          clientId: 'product',
-          brokers: [process.env.UPSTASH_KAFKA_REST_SERVER],
-          sasl: {
-            mechanism: 'scram-sha-256',
-            username: process.env.UPSTASH_KAFKA_REST_USERNAME,
-            password: process.env.UPSTASH_KAFKA_REST_PASSWORD,
-          },
-          ssl: true,
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'product',
+        brokers: [process.env.UPSTASH_KAFKA_REST_SERVER],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username: process.env.UPSTASH_KAFKA_REST_USERNAME,
+          password: process.env.UPSTASH_KAFKA_REST_PASSWORD,
         },
-        consumer: {
-          groupId: 'product-consumer'
-        }
+        ssl: true,
+      },
+      consumer: {
+        groupId: 'product-consumer'
       }
     }
-  )
-  app.listen();
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Products Service')
+    .setDescription('Products service api')
+    .setVersion('0.1')
+    .addTag('Products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.startAllMicroservices();
+  app.listen(3002);
 }
 bootstrap();
